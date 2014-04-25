1b0cacb99a636758160f2b6a557824e293840b3f
jklecker <jonathan-klecker@uiowa.edu>
2014-04-25 06:24:34 +0000

    testerr

--- app/controllers/api/v1/create_providers_controller.rb
+++ app/controllers/api/v1/create_providers_controller.rb
@@ -1,4 +1,4 @@
-class Api:::CreateProvidersController < ApplicationController
+class Api::V1::CreateProvidersController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 

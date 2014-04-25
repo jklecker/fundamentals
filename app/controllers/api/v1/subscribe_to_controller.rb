125612135c1ace0457715b62a275d1aa3283022f
jklecker <jonathan-klecker@uiowa.edu>
2014-04-25 23:55:45 +0000

    testerr

--- app/controllers/api/v1/subscribe_to_controller.rb
+++ app/controllers/api/v1/subscribe_to_controller.rb
@@ -3,13 +3,13 @@ class Api::V1::SubscribeToController < ApplicationController
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
   # Just skip the authentication for now
-   before_filter :authenticate_user!
+  # before_filter :authenticate_user!
 
   respond_to :json
   
 def create
 
-  new_subscription  = SubscribedTo.create!(:user_id => params[:user_id], :name => params[:name])
+  new_subscription  = SubscribedTo.create!(:user_id => params[:user_id], :provider_id => params[:provider_id])
     render :status => 200,
            :json => { :success => true,
                       :info => "Subscription Created",

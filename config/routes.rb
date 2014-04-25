67aa5ea5cc5e77aa20fe6acebc1cb04e55a77ffb
jklecker <jonathan-klecker@uiowa.edu>
2014-04-25 06:02:21 +0000

    testerr

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ DeviseExample::Application.routes.draw do
       end
       get 'user_providers' => 'user_providers#index'
       post 'user_providers' => 'user_providers#create'
-    end
-    post 'create_providers'=> 'create_providers#create'
+      post 'create_providers'=> 'create_providers#create'    
   end
 
   devise_for :users, :admins

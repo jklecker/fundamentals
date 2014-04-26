5bc569830982fde2517aecf9b5b5acbb3b932617
jklecker <jonathan-klecker@uiowa.edu>
2014-04-26 04:08:12 +0000

    content shit

--- app/controllers/api/v1/content_area_controller.rb
+++ app/controllers/api/v1/content_area_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ContentController < ApplicationController
+class Api::V1::ContentAreaController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 

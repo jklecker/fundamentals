92286bd928cff6aabc9490f2b1030c636bcd4f10
jklecker <jonathan-klecker@uiowa.edu>
2014-04-26 19:35:04 +0000

    content Elements

--- db/migrate/20140219023144_add_column_content_element.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnContentElement < ActiveRecord::Migration
-  def change
-    remove_column :content_elements, :provider, :references
-    add_column :content_elements, :provider_id, :integer
-  end
-end

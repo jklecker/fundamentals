246f30b24e465f624c610dd65f23ce332e88e235
jklecker <jonathan-klecker@uiowa.edu>
2014-04-26 19:51:17 +0000

    content Elements

--- app/controllers/api/v1/content_element_controller.rb
+++ app/controllers/api/v1/content_element_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ContentItemController < ApplicationController
+class Api::V1::ContentElementController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 

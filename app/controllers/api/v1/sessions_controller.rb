4772880ddab4e8351cce2e808fca6f9478c589c3
jklecker <jonathan-klecker@uiowa.edu>
2014-04-27 04:00:21 +0000

    subscribe

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,7 +9,8 @@ class Api::V1::SessionsController < Devise::SessionsController
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged in",
-                      :data => { :auth_token => current_user.authentication_token } }
+                      :data => { :auth_token => current_user.authentication_token,
+                                 :user_type => current_user.user_type} }
   end
 
   def destroy

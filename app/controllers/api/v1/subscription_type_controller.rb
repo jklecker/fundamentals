e96b8af80dcc5f6c8dfbd4154553a13dffa2ea0f
jklecker <jonathan-klecker@uiowa.edu>
2014-04-27 03:45:37 +0000

    subscribe

--- app/controllers/api/v1/subscription_type_controller.rb
+++ app/controllers/api/v1/subscription_type_controller.rb
@@ -8,14 +8,15 @@ class Api::V1::SubscriptionTypeController < ApplicationController
   respond_to :json
 
   def create
-    new_subscription_type = SubscribedType.create!(:provider_id => params[:provider_id], :name => params[:name], :subscription_length => params[:subscription_length])
+    new_subscription_type = SubscribedTypes.create!(:provider_id => params[:provider_id], :name => params[:name], :subscription_length => params[:subscription_length])
 
 	render :status => 200,
            :json => {   :success  => true,
                         :info => "Subcription Created",
                         :data => {
                         :name => new_subription_type.name,
-                        :provider_type => new_subription_type.provider.id
+                          :provider_id => new_subription_type.provider.id,
+                          :subscrition_type => new_subription_type.subscription_length
                                }
                     }
   end

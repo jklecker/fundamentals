1731848c880de4666b87bcd858f84ca8726ef4d9
jklecker <jonathan-klecker@uiowa.edu>
2014-04-27 03:58:44 +0000

    subscribe

--- app/controllers/api/v1/subscription_type_controller.rb
+++ app/controllers/api/v1/subscription_type_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::SubscriptionTypeController. < ApplicationController
+class Api::V1::SubscriptionTypeController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 

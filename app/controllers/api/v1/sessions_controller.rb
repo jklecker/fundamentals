3a7e0cc85a582c7631d7b403a275a931b99deebc
jklecker <jonathan-klecker@uiowa.edu>
2014-04-30 08:40:34 +0000

    getting user name

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::SessionsController < Devise::SessionsController
   end
   
    def index
-     user = User.where(:id => 5)
+     user = User.where(:id => 14)
 
 	render :status => 200,
            :json => { :success => true,
